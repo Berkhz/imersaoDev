693bf2a40893ef726b2be1423ecd3850f0f4089c
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-06 17:04:14 -0300

    script

---
+++ calculadoraMedia/script.js
@@ -0,0 +1,30 @@
+var nome = "Kauan"
+var notaB1 = 9
+var notaB2 = 7
+var notaB3 = 10
+var notaB4 = 6
+
+var notaFinal = (notaB1 + notaB2 + notaB3 + notaB4) / 4 
+// Soma as notas e divide para obter a média.
+var qtdDecimais = notaFinal.toFixed(1) 
+// toFixed = Exibe quantidade de casas decimais a serem computadas.
+
+console.log("Bem vindo " + nome)
+console.log(qtdDecimais)
+
+/* Revisão:
+var: Variáveis; 
+strings: "string";
+console.log;
+toFixed;
+operações matemáticas: (+,-,/,*)
+concatenação: "string" + var
+*/
+
+/* Desafios desta aula!
+Dependendo da nota, mostrar se a aluna ou aluno foi aprovada(o) ou não
+Alterar o fundo da tela da maneira que achar mais legal
+Imprimir na própria página o resultado, ao invés do console
+Criar um conversor de temperaturas entre farenheit e celcius
+Colocar a conta inteira da média em apenas uma linha
+*/
