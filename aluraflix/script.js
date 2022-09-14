895406a1a701c8b40ede7c2769b8892dd2d8e9f5
Kauan Bertalha <kauan.hb2004@gmail.com>
2022-09-14 14:23:17 -0300

    script

---
+++ aluraflix/script.js
@@ -0,0 +1,12 @@
+var listaFilmes = [
+    "https://www.themoviedb.org/t/p/original/kqdbvshn0ujV2y0N8sqm96eAsWR.jpg",
+    "https://br.web.img3.acsta.net/pictures/21/04/14/19/06/3385237.jpg",
+    "https://br.web.img3.acsta.net/pictures/19/04/26/17/30/2428965.jpg",
+    "https://ingresso-a.akamaihd.net/prd/img/movie/godzilla-vs-kong/84b7188f-de5f-4a0d-a32e-d4b49ab83131.jpg",
+    "https://br.web.img3.acsta.net/pictures/22/03/02/19/26/3666027.jpg"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
